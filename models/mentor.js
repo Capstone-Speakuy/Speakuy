994fdfcae79d59a13100f4b929efc26beaba59c4
akmalf06 <akmaldavinci06@gmail.com>
2023-06-16 17:11:00 +0700

    feat: add list my mentor

--- models/mentor.js
+++ models/mentor.js
@@ -8,7 +8,7 @@ Mentor.getRecommendations = async (userId, userText, callback) => {
     throw new Error("Mentee belum mengisi survey");
   }
   try {
-    const response = await fetch("https://bismillah-vvtmzrrdvq-ue.a.run.app/get-mentors", {
+    const response = await fetch("https://bismillahbanget-vvtmzrrdvq-ue.a.run.app//get-mentors", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -35,6 +35,15 @@ Mentor.assignToMentee = (id, mentor_id, callback) => {
       if (err) throw err;
       callback();
     }
+  )
+}
+
+Mentor.getAllMyMentor = (userId, callback) => {
+  const query = "SELECT ms.id, ms.name, ms.description FROM mentor_mentee mm JOIN mentors ms ON ms.id = mm.mentor_id JOIN mentee mt ON mt.id = mm.mentee_id WHERE mm.mentee_id = ?";
+  connection.query(
+    query,
+    [userId],
+    callback
   )
 }
 
